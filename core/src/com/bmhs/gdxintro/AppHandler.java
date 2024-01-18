af5a6b21141987fb1100cbdbf964880b8e38962c
axreynolds456 <aidanxreynolds@gmail.com>
2024-01-18 13:28:48 -0800

    first commit

--- core/src/com/bmhs/gdxintro/AppHandler.java
+++ core/src/com/bmhs/gdxintro/AppHandler.java
@@ -6,19 +6,17 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.bmhs.gdxintro.gfx.utils.TileHandler;
 
 public class AppHandler extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	TextureRegion subImg;
 	int x, y;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("primaryColorSheet.png");
-		subImg = new TextureRegion(img, 64, 0, 128, 128);
 
 		x = 0;
 		y = 0;
@@ -29,7 +27,15 @@ public class AppHandler extends ApplicationAdapter {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 
-		batch.draw(subImg, x, y);
+		for(int r = 0; r < Gdx.graphics.getHeight(); r+=64) {
+			for(int c = 0; c < Gdx.graphics.getWidth(); c+=64) {
+				batch.draw(TileHandler.getTileHandler().getWorldTileArray().get(x++).getTexture(), c, r);
+				if(x > 4) {
+					x = 0;
+				}
+			}
+		}
+
 
 		batch.end();
 
@@ -37,6 +43,7 @@ public class AppHandler extends ApplicationAdapter {
 	}
 
 	public void checkInput() {
+
 		/*
 		if(Gdx.input.isKeyJustPressed(Input.Keys.W)) {
 			y += 10;
@@ -56,26 +63,25 @@ public class AppHandler extends ApplicationAdapter {
 
 		x = Gdx.input.getX() - img.getWidth()/2;
 		y = Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2;
-		 */
+
+
 		if(x < (Gdx.input.getX() - img.getWidth()/2)) {
-			x += 20;
+			x += 5;
 		}
-		else if(x > (Gdx.input.getX() - img.getWidth()/2)) {
-			x -= 20;
+		if(x > (Gdx.input.getX() - img.getWidth()/2)) {
+			x -= 5;
 		}
-		else if(y < (Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2)) {
-			y += 20;
+		if(y < (Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2)) {
+			y += 5;
 		}
-		else if(y > (Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2)) {
-			y -= 20;
+		if(y > (Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2)) {
+			y -= 5;
 		}
-
-
+*/
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
